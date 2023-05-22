9dc137c80a9051bfa81b26bfc6510f8aa87ab61d
CuSO4-c3c <77104443+CuSO4-c3c@users.noreply.github.com>
2023-05-22 13:15:43 +0700

    t co khien

---
+++ command/echo.js
@@ -0,0 +1,23 @@
+const info = [
+    {
+      name: 'echo',
+      description: 'Repeat what you said',
+      options: [
+        {
+            name: 'input',
+            description: 'Say something here',
+            type: 3,
+            required: true,
+        }
+      ]
+    },
+];
+async function cmd(interaction) {
+    const input = interaction.options.getString('input');
+    await interaction.reply(`You just said: ${input}`);
+}
+
+module.exports = {
+    info: info,
+    exec: cmd
+}
